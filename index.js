6c1dc106858b89a789eb5680604b42b376374f32
kanak56 <ckanak40@gmail.com>
2021-10-30 22:21:24 +0600

    second

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.port || 5000;
+const { MongoClient } = require('mongodb');
 require('dotenv').config()
 
+// uri mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kadog.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri);
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
