e70c483bbd398f67f83854777c4eba0919c30239
kanak56 <ckanak40@gmail.com>
2021-11-01 08:59:48 +0600

    last

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
+require('dotenv').config()
 const ObjectId = require('mongodb').ObjectId;
 const { MongoClient } = require('mongodb');
 
 // midleware
 app.use(cors());
 app.use(express.json());
-require('dotenv').config()
+
 
 // uri mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kadog.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
